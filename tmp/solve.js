55c5bd70dc9a73c8b2b7285e8c905dba5750cecd
Sebastian Ullrich <sebastian.ullrich@deepsource.de>
2023-11-19 13:48:50 +0100

    feat: initial commit

---
+++ tmp/solve.js
@@ -0,0 +1,28 @@
+import _ from "lodash"
+import data from "./data.js"
+import sample from "./sample.js"
+
+console.log("🎄 Day XXX: YYY")
+
+const skip1 = false
+const skip2 = true
+
+/// Part 1
+
+!skip1 && console.log("\nPart 1:")
+
+const solve1 = (data) => {
+}
+
+!skip1 && console.log("Sample:\t", solve1(sample))
+!skip1 && console.log("Task:\t", solve1(data))
+
+/// Part 2
+
+!skip2 && console.log("\nPart 2:")
+
+const solve2 = (data) => {
+}
+
+!skip2 && console.log("Sample:\t", solve2(sample))
+!skip2 && console.log("Task:\t", solve2(data))
