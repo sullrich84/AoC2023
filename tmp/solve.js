54352860f5af7544bb4dac365a84f54761ca1d8e
Sebastian Ullrich <sebastian.ullrich@deepsource.de>
2023-11-26 23:12:06 +0100

    chore: update solve

--- tmp/solve.js
+++ tmp/solve.js
@@ -5,25 +5,27 @@ import sample from "./sample.js"
 console.clear()
 console.log("🎄 Day XXX: YYY")
 
-const skip1 = false
-const skip2 = true
+const run1 = true
+const run2 = false
 
 /// Part 1
 
-!skip1 && console.log("\nPart 1:")
+const solve1 = (data) => {}
 
-const solve1 = (data) => {
-}
+const solve1Sample = run1 ? solve1(sample) : "skipped"
+const solve1Data = run1 ? solve1(data) : "skipped"
 
-!skip1 && console.log("Sample:\t", solve1(sample))
-!skip1 && console.log("Task:\t", solve1(data))
+console.log("\nPart 1:")
+console.log("Sample:\t", solve1Sample)
+console.log("Task:\t", solve1Data)
 
 /// Part 2
 
-!skip2 && console.log("\nPart 2:")
+const solve2 = (data) => {}
 
-const solve2 = (data) => {
-}
+const solve2Sample = run2 ? solve2(sample) : "skipped"
+const solve2Data = run2 ? solve2(data) : "skipped"
 
-!skip2 && console.log("Sample:\t", solve2(sample))
-!skip2 && console.log("Task:\t", solve2(data))
+console.log("\nPart 2:")
+console.log("Sample:\t", solve2Sample)
+console.log("Task:\t", solve2Data)
