847f44dee076ea7c3a71df4dbf5e5c092b912308
Sebastian Ullrich <sebastian.ullrich@deepsource.de>
2023-12-08 08:47:50 +0100

    chore: cleanup

--- day6/solve.ts
+++ day6/solve.ts
@@ -1,10 +1,6 @@
 // @deno-types="npm:@types/lodash"
-import _, { countBy } from "npm:lodash"
+import _ from "npm:lodash"
 import { data1, data2, Puzzle, sample1, sample2 } from "./data.ts"
-import { ValueStore, wait } from "../utils/utils.ts"
-import { Counter } from "../utils/counter.ts"
-// const data = Deno.readTextFileSync("./day6/task.txt")
-// const input = Deno.readTextFileSync("./day6/sample.txt")
 
 console.clear()
 console.log("🎄 Day 6: Wait For It")
@@ -17,7 +13,6 @@ const runBoth = true
 
 const solve = (data: Puzzle) => {
   const results = []
-  const counter = new Counter
 
   for (let race = 0; race < data.length; race++) {
     const ttb = data[race].ms
