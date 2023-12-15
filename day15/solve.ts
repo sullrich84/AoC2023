3786ec7f1a3908ce7253ec663b4c195c47a131fa
Sebastian Ullrich <sebastian.ullrich@deepsource.de>
2023-12-15 08:49:05 +0100

    feat: day 15

--- day15/solve.ts
+++ day15/solve.ts
@@ -2,31 +2,26 @@
 import _ from "npm:lodash"
 import { read } from "../utils/Reader.ts"
 
-type Puzzle = string[][]
+type Puzzle = string[]
 
-const [task, sample] = read("day15").map((file) => file.replace("\n", "").split(","))
+const [task, sample] = read("day15").map((file) =>
+  file.replace("\n", "").split(",")
+)
 
 console.clear()
 console.log("🎄 Day 15: Lens Library")
 
 const runPart1 = true
-const runPart2 = false
+const runPart2 = true
 const runBoth = true
 
 /// Part 1
 
 function hash(input: string, val: number): number {
   if (input == "") return val
-
-  const [char, rest] = [input.substring(0, 1), input.substring(1)]
-  const code = char.charCodeAt(0)
-  // console.log("CHAR:", char, val, "CODE:", code, "REST:", rest || "-empty-")
-
-  val += code
-  val *= 17
-  val %= 256
-
-  return hash(rest, val)
+  const code = input.charCodeAt(0)
+  const rest = input.substring(1)
+  return hash(rest, (val + code) * 17 % 256)
 }
 
 const solve1 = (data: Puzzle) => {
@@ -42,11 +37,50 @@ console.log("Task:\t", solve1Data)
 
 /// Part 2
 
-function hashMap(input: string) {
+type Boxes = { [key: string]: { [key: string]: string } }
 
+function hashMap(input: string, boxes: Boxes) {
+  const [label] = input.split(/-|=/)
+  const box = hash(label, 0)
+
+  // Initalize empty (new) box
+  if (_.isUndefined(boxes[box])) boxes[box] = {}
+
+  if (input.includes("-")) {
+    boxes[box] = _.omit(boxes[box], label)
+    return boxes
+  }
+
+  // Replace label or append at end
+  const focal = _.last(input.split("="))
+  boxes[box][label] = focal!
+  return boxes
+}
+
+function focusPower(boxes: Boxes) {
+  let focusingPower = 0
+
+  for (const [boxNumber, boxContent] of _.entries(boxes)) {
+    const box = parseInt(boxNumber)
+    let slotCount = 0
+
+    for (const [_label, focusLength] of _.entries(boxContent)) {
+      const focal = parseInt(focusLength)
+      focusingPower += (box + 1) * ++slotCount * focal
+    }
+  }
+
+  return focusingPower
 }
 
 const solve2 = (data: Puzzle) => {
+  let boxes = {}
+
+  for (const input of data) {
+    boxes = hashMap(input, boxes)
+  }
+
+  return focusPower(boxes)
 }
 
 const solve2Sample = runPart2 ? solve2(sample) : "skipped"
