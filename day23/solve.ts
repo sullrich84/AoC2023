78642de40ac0ffa6ea8adafad8cf8bf1ad831edd
Sebastian Ullrich <sebastian.ullrich@deepsource.de>
2023-12-23 08:33:47 +0100

    chore: part 1

--- day23/solve.ts
+++ day23/solve.ts
@@ -15,8 +15,8 @@ const [task, sample] = read("day23").map((file) =>
 console.clear()
 console.log("🎄 Day 23: A Long Walk")
 
-const runPart1 = true
-const runPart2 = false
+const runPart1 = false
+const runPart2 = true
 const runBoth = true
 
 /// Part 1
@@ -64,7 +64,7 @@ const solve1 = (data: Puzzle) => {
 
   let maxSteps = Number.NEGATIVE_INFINITY
   while (stack.length > 0) {
-    const [y, x, ty, tx, seen] = stack.shift()!
+    const [y, x, ty, tx, seen] = stack.pop()!
     // draw(data, y, x, ty, tx)
 
     // Check if target reached
@@ -110,6 +110,48 @@ console.log("Task:\t", solve1Data)
 /// Part 2
 
 const solve2 = (data: Puzzle) => {
+  const [yLen, xLen] = [data.length, data[0].length]
+
+  const stack: Stack = []
+  stack.push([0, 1, yLen - 1, xLen - 2, new Set()])
+
+  let maxSteps = Number.NEGATIVE_INFINITY
+  while (stack.length > 0) {
+    const [y, x, ty, tx, seen] = stack.pop()!
+    // draw(data, y, x, ty, tx)
+
+    // Check if target reached
+    if (y == ty && x == tx) {
+      console.log(`Target Reached with ${seen.size} steps`)
+      maxSteps = Math.max(maxSteps, seen.size)
+      continue
+    }
+
+    // Update state
+    const nSeen = new Set(seen)
+    const key = [y, x].join(":")
+    nSeen.add(key)
+
+    // Move
+    let tile = data[y][x]
+    if (tile == ">" || tile == "v") tile = "."
+    const nDir = dir[tile]
+
+    for (const [dy, dx] of nDir) {
+      const [ny, nx] = [y + dy, x + dx]
+      if (!_.inRange(ny, 0, yLen) || !_.inRange(nx, 0, xLen)) continue // out of bounds
+
+      const nTile = data[ny][nx]
+      if (nTile == "#") continue // skip forest
+
+      const nKey = [ny, nx].join(":")
+      if (nSeen.has(nKey)) continue // already entered
+
+      stack.push([ny, nx, ty, tx, nSeen])
+    }
+  }
+
+  return maxSteps
 }
 
 const solve2Sample = runPart2 ? solve2(sample) : "skipped"
